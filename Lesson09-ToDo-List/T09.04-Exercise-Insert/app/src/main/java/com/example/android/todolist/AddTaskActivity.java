226c443a1040f79546ef6148a0460c21585e08ba
Nkonda <nkonda@roamdata.com>
2018-01-16 23:18:17 -0500

    Finished  T09.04-Exercise-Insert

--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -16,21 +16,31 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.example.android.todolist.data.TaskContract;
+
+import javax.xml.datatype.Duration;
 
 
 public class AddTaskActivity extends AppCompatActivity {
 
     // Declare a member variable to keep track of a task's selected mPriority
     private int mPriority;
+    EditText mTaskDescEditText;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        mTaskDescEditText = (EditText) findViewById(R.id.editTextTaskDescription);
 
         // Initialize to highest mPriority by default (mPriority = 1)
         ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
@@ -43,14 +53,18 @@ public class AddTaskActivity extends AppCompatActivity {
      * It retrieves user input and inserts that new task data into the underlying database.
      */
     public void onClickAddTask(View view) {
-        // Not yet implemented
-        // TODO (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
-
-        // TODO (7) Insert new task data via a ContentResolver
-
-        // TODO (8) Display the URI that's returned with a Toast
-        // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
-
+        String taskDesc = mTaskDescEditText.getText().toString();
+        if (taskDesc.isEmpty()) {
+            return;
+        }
+        ContentValues values = new ContentValues();
+        values.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, taskDesc);
+        values.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
+        Uri returnedUri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, values);
+        if (returnedUri != null) {
+            Toast.makeText(this, returnedUri.toString(), Toast.LENGTH_SHORT).show();
+        }
+        finish();
     }
 
 
